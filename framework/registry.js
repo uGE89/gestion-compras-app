db8ebceef73096cbd32cc7c96d1abae7e733a5a1
uGE89 <uge300889@gmail.com>
2025-09-08 12:28:49 -0600

    Remove template and missing app entries from registry

--- framework/registry.js
+++ framework/registry.js
@@ -1,10 +1,6 @@
 export const registry = {
-  plantilla:  () => import('../apps/plantilla.app.js'),
-  miapp:      () => import('../apps/miapp.app.js'), // ← nuevo
   'analizador-compra': () => import('../apps/analizador_compra.app.js'),
   'creador-pedido': () => import('../apps/creador_pedido.app.js'),
   'detalles': () => import('../apps/detalles_articulo.app.js'),
   'mis_pedidos': () => import('../apps/mis_pedidos.app.js'),
-
-
 };
