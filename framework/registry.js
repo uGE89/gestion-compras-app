e741391432f924c9defc28c7befa7abf1edf37c4
uGE89 <uge300889@gmail.com>
2025-09-09 12:04:03 -0600

    Update registry.js

--- framework/registry.js
+++ framework/registry.js
@@ -3,4 +3,8 @@ export const registry = {
   'creador-pedido': () => import('../apps/creador_pedido.app.js'),
   'detalles': () => import('../apps/detalles_articulo.app.js'),
   'mis_pedidos': () => import('../apps/mis_pedidos.app.js'),
+  'compras_historial': () => import('../apps/compras_historial.app.js'),
+  'compras_registrar': () => import('../apps/compras_registrar.app.js'),
+  'compras_detalles': () => import('../apps/compras_detalles.app.js'),
+  'compras_editar': () => import('../apps/compras_editar.app.js'),  
 };
