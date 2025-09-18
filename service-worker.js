2a768e5471ac50782181aaf914db6d4df55d46e6
uGE89 <uge300889@gmail.com>
2025-09-17 23:49:47 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 // ⇨ Sube este número en cada deploy para invalidar caché viejo.
-const APP_VERSION = '2025.09.1.4';
+const APP_VERSION = '2025.09.1.151';
 const PREFIX     = 'gestion-compras-cache-';
 const CACHE_NAME = `${PREFIX}${APP_VERSION}`;
 
